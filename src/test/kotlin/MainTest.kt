f8c0c9bc20395beba7939e81f38febd893d79c56
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-10-21 17:38:41 +0100

    🎉 initial commit

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun foo() {
+        assertThat("Foo").isEqualTo("Foo")
+    }
+
+}
