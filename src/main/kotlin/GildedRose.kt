932f17f37132d9280712a0e936288bc3cbeaa194
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-10-22 12:02:38 +0100

    ♻ reorder the method

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -4,16 +4,13 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         items.forEach { item ->
             val maxQuality = 50
-            val minQuality = 0
             val agedBrie = "Aged Brie"
             val backstagePasses = "Backstage passes to a TAFKAL80ETC concert"
             val qualityIncrease = 1
-            val qualityDecrease = 1
             val isNotTheMaximumQuality = item.quality < maxQuality
             val isAgedBrie = item.name == agedBrie
             val isABackStagePass = item.name == backstagePasses
             val areAgeBrieOrPasses = isAgedBrie || isABackStagePass
-            val hasQuality = item.quality > minQuality
             if (areAgeBrieOrPasses) {
                 if (isNotTheMaximumQuality) {
                     item.quality += qualityIncrease
@@ -21,38 +18,38 @@ class GildedRose(var items: List<Item>) {
                         val backstagePassesBigThreshold = 11
                         val areInDateToBeSold = item.sellIn < backstagePassesBigThreshold
                         if (areInDateToBeSold && isNotTheMaximumQuality) {
-                            item.quality = item.quality + qualityIncrease
+                            item.quality += qualityIncrease
                         }
 
                         val backstagePassesSmallThreshold = 6
                         val areOnTheLastDays = item.sellIn < backstagePassesSmallThreshold
                         if (areOnTheLastDays && isNotTheMaximumQuality) {
-                            item.quality = item.quality + qualityIncrease
+                            item.quality += qualityIncrease
                         }
                     }
                 }
             }
-            if (!areAgeBrieOrPasses && hasQuality && isNotSulfuras(item)) {
-                item.quality -= qualityDecrease
-            }
-
-
             decreaseSellIn(item)
-
             val notExpiredNumber = 0
             val isExpired = item.sellIn < notExpiredNumber
-
             if(isExpired && isAgedBrie && isNotTheMaximumQuality){
                 item.quality += qualityIncrease
             }
 
+
+            val qualityDecrease = 1
+            val minQuality = 0
+            val hasQuality = item.quality > minQuality
+            if (!areAgeBrieOrPasses && hasQuality && isNotSulfuras(item)) {
+                item.quality -= qualityDecrease
+            }
             if (isExpired) {
                 if (!isAgedBrie) {
                     if (isABackStagePass) {
-                        item.quality = item.quality - item.quality
+                        item.quality -= item.quality
                     } else {
                         if (hasQuality && isNotSulfuras(item)) {
-                            item.quality = item.quality - qualityDecrease
+                            item.quality -= qualityDecrease
                         }
                     }
                 }
