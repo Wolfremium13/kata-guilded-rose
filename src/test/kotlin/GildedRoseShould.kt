304f0c58a810eaddadd0201d64fe4741d75611f4
Luis Rodríguez <luis.rodriguez@leanmind.es>
2024-01-26 16:56:53 +0100

    reduce code, parameters are modified by reference

--- src/test/kotlin/GildedRoseShould.kt
+++ src/test/kotlin/GildedRoseShould.kt
@@ -17,64 +17,57 @@ class GildedRoseShould {
     )
     fun `quality cannot be negative`(itemName: String) {
         val item = Item(name = itemName, sellIn = 0, quality = 0)
-        val shop = GildedRose(listOf(item))
 
-        shop.updateQuality()
+        GildedRose(listOf(item)).updateQuality()
 
-        assertThat(shop.items[0].quality).isEqualTo(0)
+        assertThat(item.quality).isEqualTo(0)
     }
 
     @Test
     fun `Aged Brie quality cannot be higher than the maximum`() {
         val MAXIMUM_QUALITY = 50
         val item = Item(name = "Aged Brie", sellIn = -1, quality = MAXIMUM_QUALITY)
-        val shop = GildedRose(listOf(item))
 
-        shop.updateQuality()
+        GildedRose(listOf(item)).updateQuality()
 
-        assertThat(shop.items[0].quality).isEqualTo(MAXIMUM_QUALITY)
+        assertThat(item.quality).isEqualTo(MAXIMUM_QUALITY)
     }
 
     @Test
     fun `not allow higher than the maximum quality for Backstage passes`() {
         val item = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 2, quality = 50)
-        val shop = GildedRose(listOf(item))
 
-        shop.updateQuality()
+        GildedRose(listOf(item)).updateQuality()
 
-        assertThat(shop.items[0].quality).isEqualTo(50)
+        assertThat(item.quality).isEqualTo(50)
     }
 
     @Test
     fun `not allow change the quality of Sulfuras`() {
-        val item = Item(name = "Sulfuras, Hand of Ragnaros", sellIn = 1, quality = 80)
-        val shop = GildedRose(listOf(item))
+        val item = Item(name = "Sulfuras, Hand of Ragnaros", sellIn = 1, quality = 20)
 
-        shop.updateQuality()
+        GildedRose(listOf(item)).updateQuality()
 
-        assertThat(shop.items[0].quality).isEqualTo(80)
+        assertThat(item.quality).isEqualTo(20)
     }
 
     @Test
     fun `allow to change the quality of Backstage passes`(){
         val item = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 0, quality = 50)
-        val shop = GildedRose(listOf(item))
 
-        shop.updateQuality()
+        GildedRose(listOf(item)).updateQuality()
 
-        assertThat(shop.items[0].quality).isEqualTo(0)
-        assertThat(shop.items[0].sellIn).isEqualTo(-1)
+        assertThat(item.quality).isEqualTo(0)
+        assertThat(item.sellIn).isEqualTo(-1)
     }
 
     @Test
     fun `decrease twice time faster the expired items` (){
         val item = Item(name = "DefaultIteam", sellIn = 0, quality = 50)
-        val shop = GildedRose(listOf(item))
 
-        shop.updateQuality()
+        GildedRose(listOf(item)).updateQuality()
 
-        assertThat(shop.items[0].quality).isEqualTo(48)
-        assertThat(shop.items[0].sellIn).isEqualTo(-1)
+        assertThat(item.quality).isEqualTo(48)
+        assertThat(item.sellIn).isEqualTo(-1)
     }
-
 }
