1caa93d845a39dde4389b5f310a675a2ad98296a
Luis Rodríguez <luis.rodriguez@leanmind.es>
2024-01-26 17:50:02 +0100

    refactor: remove else branch

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -42,9 +42,10 @@ class GildedRose(var items: List<Item>) {
             if (item.name != "Aged Brie") {
                 if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
                     if (item.quality > 0) {
-                        if (item.name != "Sulfuras, Hand of Ragnaros") {
-                            item.quality = item.quality - 1
+                        if (item.name == "Sulfuras, Hand of Ragnaros") {
+                            return
                         }
+                        item.quality = item.quality - 1
                     }
                 } else {
                     item.quality = item.quality - item.quality
