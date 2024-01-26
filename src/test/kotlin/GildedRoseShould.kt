78f9a793beef8a877f53d1b059fac0caebadf531
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-01-26 13:28:58 +0000

    ✅ added tests

--- src/test/kotlin/GildedRoseShould.kt
+++ src/test/kotlin/GildedRoseShould.kt
@@ -2,7 +2,46 @@ import dev.wolfremium.www.GildedRose
 import dev.wolfremium.www.Item
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class GildedRoseShould {
+    // The system cannot support out of edge inputs
 
+    @ParameterizedTest(name = "quality of {0} cannot be negative")
+    @ValueSource(
+        strings = [
+            "Backstage passes",
+            "DefaultItem"
+        ]
+    )
+    fun `quality cannot be negative`(itemName: String) {
+        val item = Item(name = itemName, sellIn = 1, quality = 0)
+        val shop = GildedRose(listOf(item))
+
+        shop.updateQuality()
+
+        assertThat(shop.items[0].quality).isEqualTo(0)
+    }
+
+    @Test
+    fun `Aged Brie quality cannot be higher than the maximum`() {
+        val MAXIMUM_QUALITY = 50
+        val item = Item(name = "Aged Brie", sellIn = -1, quality = MAXIMUM_QUALITY)
+        val shop = GildedRose(listOf(item))
+
+        shop.updateQuality()
+
+        assertThat(shop.items[0].quality).isEqualTo(MAXIMUM_QUALITY)
+    }
+
+    @Test
+    fun `quality cannot be higher than 50 for Backstage passes`() {
+        val item = Item(name = "Backstage passes", sellIn = 1, quality = 50)
+        val shop = GildedRose(listOf(item))
+
+        shop.updateQuality()
+
+        assertThat(shop.items[0].quality).isEqualTo(50)
+    }
 }
