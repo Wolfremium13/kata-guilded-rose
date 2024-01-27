12ed3bbe861da9eaaccbf87c67d1c4a2607ff807
Luis Rodríguez <seymourpoler@gmail.com>
2024-01-27 22:41:32 +0100

    refactor: remove indentation

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -34,15 +34,18 @@ class GildedRose(var items: List<Item>) {
                     item.quality = item.quality + 1
                 }
             }
+            item.sellIn = item.sellIn - 1
+            if (item.sellIn < 0) {
+                item.quality = item.quality - item.quality
+            }
+
+            return
         }
 
         item.sellIn = item.sellIn - 1
 
         if (item.sellIn < 0) {
-            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                item.quality = item.quality - item.quality
-                return
-            }
+
 
             if (item.name == "Aged Brie") {
                 if (item.quality < 50) {
